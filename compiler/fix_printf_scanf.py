f03f0e13355a82ab08e6dc71b10b244ab3f8dfbe
LAZ <sdf10528236@gmail.com>
2022-02-25 18:02:13 +0800

    fix_printf_scanf

--- compiler/fix_printf_scanf.py
+++ compiler/fix_printf_scanf.py
@@ -5,6 +5,33 @@ from difflib import SequenceMatcher
 import regex
 
 
+def fix_str(old_file, new_file, printf_fix_line, scanf_fix_line):
+    line_column = 1
+    file_data = ""
+    with open(old_file, "r") as f:
+
+        for line in f:
+            if str(line_column) in printf_fix_line:
+                line = line.replace(
+                    printf_fix_line[str(line_column)], "printf")
+            elif str(line_column) in scanf_fix_line:
+                line = line.replace(
+                    scanf_fix_line[str(line_column)], "scanf")
+            file_data += line
+
+            line_column = line_column+1
+    with open(new_file, "w") as f:
+        f.write(file_data)
+
+
+def run_compiler(path):
+    p = subprocess.run(
+        ["D:/Program Files/CodeBlocks/MinGW/bin/gcc.exe", path], capture_output=True)
+    print(p.stderr.decode("utf-8"))
+    warning_text = p.stderr.decode("utf-8").splitlines()
+    return warning_text
+
+
 def str_warning(warning_text):
     printf_fix_line = {}
     scanf_fix_line = {}
@@ -50,26 +77,21 @@ if __name__ == '__main__':
     fileList = os.listdir(file_path)
     for file in fileList:
         filename = file
+        warning_text = run_compiler(f"../data/wrong_data/{filename}")
 
-        p = subprocess.run(
-            ["D:/Program Files/CodeBlocks/MinGW/bin/gcc.exe", f"../data/wrong_data/{filename}"], capture_output=True)
-        print(p.stderr.decode("utf-8"))
-        warning_text = p.stderr.decode("utf-8").splitlines()
         printf_fix_line, scanf_fix_line = str_warning(warning_text)
 
-        line_column = 1
-        file_data = ""
-        with open(f"../data/wrong_data/{filename}", "r") as f:
+        fix_str(f"../data/wrong_data/{filename}",
+                f"../data/correct_data/c{filename}", printf_fix_line, scanf_fix_line)
 
-            for line in f:
-                if str(line_column) in printf_fix_line:
-                    line = line.replace(
-                        printf_fix_line[str(line_column)], "printf")
-                elif str(line_column) in scanf_fix_line:
-                    line = line.replace(
-                        scanf_fix_line[str(line_column)], "scanf")
-                file_data += line
+    # file_path = 'D:/program projects/coderepair/data/correct_data/'
+    # fileList = os.listdir(file_path)
+    # for file in fileList:
+    #     filename = file
+    #     for i in range(5):
+    #         warning_text = run_compiler(f"../data/correct_data/{filename}")
 
-                line_column = line_column+1
-        with open(f"../data/correct_data/c{filename}", "w") as f:
-            f.write(file_data)
+    #         printf_fix_line, scanf_fix_line = str_warning(warning_text)
+
+    #         fix_str(f"../data/correct_data/{filename}",
+    #                 f"../data/correct_data/{filename}", printf_fix_line, scanf_fix_line)
