505526ce768716b746be3c45b3e6315f7b659d06
sdf10528236 <sdf10528236@gmail.com>
2022-03-16 16:24:32 +0800

    a

--- compiler/compiler_mesg.py
+++ compiler/compiler_mesg.py
@@ -22,43 +22,100 @@ def run_compiler(args): #filepath, compiler_path="gcc"):
     if args.file:
         result = compile_file(args.file)
         print(result)
-        check_printf(result)
+        
     elif args.idir:
         for file in get_dir_files(args.idir):
             print(file)
             result = compile_file(os.path.join(args.idir, file))
-            print("  ", result)
-            if check_printf(result):
-                shutil.copyfile(f"{args.idir}/{file}",f"pwdata/{file}")
+            print (result)
+            column = find_column(result,file)
+            find_printf_line(os.path.join(args.idir, file),column)
+            
 
 
-def check_printf(result):
-    check = 1 
+def find_printf_line(file,column):
+    print(column)
+    line_column = 1
+    
+    with open(file, "r") as f:
+
+        for line in f:
+            if str(line_column) in column:
+                printf_positions = [m.span()
+                                    for m in regex.finditer('printf', line)]
+                if(len(printf_positions) > 0):
+                    
+                    
+                   
+                    
+                    fix_line = line[printf_positions[0][0]:]
+                    print(fix_line)
+
+                    check_printf_error(fix_line)
+
+            
+
+            line_column = line_column+1
+        #print(file_data)
+            
+def check_printf_error(line):
+    file_data ='''
+        #include <stdio.h>
+        int main()
+        {
+            
+        
+        '''+f"{line}"+'''    return 0;
+        }'''
+
+
+    print(file_data)
+    file_name = "test/temp.c"
+    with open(file_name, "w") as f:
+        f.write(file_data)
+    result = compile_file(file_name)
     for text in result:
-        #print(text)
-        if check:
-            mean_position = printf_positions = [m.span()
-                            for m in regex.finditer('mean', text)]
-            printf_positions = [m.span()
-                            for m in regex.finditer('printf', text)]
-            ismean = len (mean_position)
-            isprintf = len(printf_positions)
-            if isprintf:
-                if ismean:
-                    print("i find it !!!!!!!!")
-                    return 1
-                else:
-                    check = 0 
-        else:
-            p_up = text.find('^')
-            if p_up >= printf_positions[0][0]:
-                check = 1
-                print("i find it !!!!!!!!")
-                return 1
+        error_p = [m.span()for m in regex.finditer('error', text)]
+        warning_p = [m.span()for m in regex.finditer('warning', text)]
+        if(len(error_p ) or len(warning_p)):
+            undeclared_p = [m.span()
+                               for m in regex.finditer("undeclared", text)]
+            if(len(undeclared_p)):
+                print(text)
             else:
-                check =1  
+                print("!!!!!!!!!!!!!!!!!!!!")
+                print(text)
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+              
+
+        
+
+             
+        
             
+def find_column(warning_text, filename):
+    column = []
+    for text in warning_text:
+        print(text)
+        error_p = [m.span()for m in regex.finditer('error', text)]
+        warning_p = [m.span()for m in regex.finditer('warning', text)]
+        if(len(error_p ) or len(warning_p)):
+            # print(text)
+            colon_positions = [m.span()
+                               for m in regex.finditer(":", text)]  # 冒號位置
+            colume_start = [m.span()
+                            for m in regex.finditer(f"{filename}:", text)]
             
+            if(len(colon_positions) and len(colume_start)):
+                column_end = min((i[0] for i in colon_positions if i[0] >
+                              colume_start[0][1]), key=lambda x: abs(x - colume_start[0][1]))
+                new_line = text[colume_start[0][1]:column_end]
+                if new_line not in column:
+
+                    column.append(new_line) 
+            
+
+    return column
    
 
 
--- compiler/test/c3.c
+++ compiler/test/c3.c
@@ -6,6 +6,7 @@ int main()
 	scanf("%d", &n);
 	for (int i=0; i<n; i++) scanf("%d", &a[i]);
 	for (int i=n-1; i>0; i--) printf("%d ", a[i]);
-	for ( int i=0; i>-1; i--) printf("%d", a[i]) ;        scanf("%d" &a[i]);                   
+	for ( int i=0; i>-1; i--) printf("%d", a[i]) ;       
+	for ( int i=0; i>-1; i--) printf("%d", a[j]) ;            
 	return 0;
 }
