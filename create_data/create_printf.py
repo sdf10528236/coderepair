f1bddf7b6a13b5d022d14b4dcbe53ae54d5ceb38
sdf10528236 <sdf10528236@gmail.com>
2022-03-17 14:39:46 +0800

    create data improve

--- create_data/create_printf.py
+++ create_data/create_printf.py
@@ -1,7 +1,16 @@
-import numbers
+import random
 import string
 import random
 import regex
+def printf_add_word(cur_line_str):
+    WORDS = ("python", "good", "easy", "difficult", "Hello world!", "hello")
+    word = random.choice(WORDS)
+    positions = [m.span()
+                 for m in regex.finditer("\"", cur_line_str)]
+    
+    to_corrupt = positions[0]  # 第一個"的地方
+    cur_line_str = cur_line_str[:to_corrupt[0]+1]+word+cur_line_str[to_corrupt[0]+1:]
+    print(cur_line_str)
 
 
 def printf_add_parameter(cur_line_str, numbers):
@@ -22,9 +31,7 @@ def printf_add_parameter(cur_line_str, numbers):
     cur_line_str = cur_line_str + "\""
     for i in range(numbers):
         cur_line_str = cur_line_str + ", "+"".join(random.choice(
-            string.ascii_letters)) + "["+"".join(random.choice(
-                string.ascii_letters)) + "]"
-
+            string.ascii_letters.lower())) 
     cur_line_str = cur_line_str + ");"
     print(cur_line_str)
     return cur_line_str
@@ -33,7 +40,7 @@ def printf_add_parameter(cur_line_str, numbers):
 def creat_printf(numbers, number_of_strings=10):  # 產生printf("字串")程式碼
     cur_line_str = []
     for i in range(numbers):
-        cur_line_str.append("printf(\""+"".join(random.choice(string.ascii_letters)
+        cur_line_str.append("printf(\""+"".join(random.choice(string.ascii_letters.lower())
                                                 for _ in range(random.randint(0, number_of_strings)))+"\");")
 
     return cur_line_str
@@ -44,7 +51,9 @@ if __name__ == '__main__':
     cur_line_strs = creat_printf(10, 0)
     print(len(cur_line_strs))
     for i in range(len(cur_line_strs)):
-        cur_line_strs[i] = printf_add_parameter(
-            cur_line_strs[i], random.randint(1, 2))
-
-    print(cur_line_strs)
+        # cur_line_strs[i] = printf_add_parameter(
+        #     cur_line_strs[i], random.randint(0, 4))
+        
+        cur_line_strs[i] = printf_add_word(
+            cur_line_strs[i])
+    #print(cur_line_strs)
