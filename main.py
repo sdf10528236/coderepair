54b7cc457e9122264e834cd7bddf428d65174c22
sdf10528236 <sdf10528236@gmail.com>
2022-03-24 12:06:53 +0800

    a

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from compiler.fix_printf_scanf import auto_fix_str
 from model_test.model_fix import auto_model_fix
-from compiler.compiler_mesg import compiler_mesg
+from compiler.find_printf_error_data import compiler_mesg
 
 if __name__ == '__main__':
 
