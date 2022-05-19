af9bc3e9a72f0728bfdf8fcfda2a4a8bb2682064
sdf10528236 <sdf10528236@gmail.com>
2022-05-19 15:58:25 +0800

    a

--- count.py
+++ count.py
@@ -15,8 +15,8 @@ dir = "data/ffdata_2"   #修復失敗程式碼的資料夾
 for path in os.listdir(dir):
     if os.path.isfile(os.path.join(dir, path)):
         fail_count += 1
-print("總資料量：",correct_count+fail_count)
-print("成功修:",correct_count)
-print("失敗修：",fail_count)
 
+print("總資料量：",correct_count+fail_count)
+print("成功修復:",correct_count)
+print("失敗修復：",fail_count)
 print("修復率：",correct_count/(correct_count+fail_count))
