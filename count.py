d283c1dc1c09289a65b836258b6a727a67bd41de
sdf10528236 <sdf10528236@gmail.com>
2022-05-20 18:02:40 +0800

    a

--- count.py
+++ count.py
@@ -18,7 +18,7 @@ for path in os.listdir(dir):
     if os.path.isfile(os.path.join(dir, path)):
         fail_count += 1
 
-print("總資料量：",correct_count+fail_count)
+print("總修復資料量：",correct_count+fail_count)
 print("成功修復:",correct_count)
 print("失敗修復：",fail_count)
 print("修復率：",correct_count/(correct_count+fail_count))
