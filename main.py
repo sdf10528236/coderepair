c2cf8ec4fbe488eca8f37524cf9a1d19932df571
sdf10528236 <sdf10528236@gmail.com>
2022-05-20 16:48:22 +0800

    q

--- main.py
+++ main.py
@@ -91,8 +91,8 @@ def code_fix(file_path,filename):
 
 if __name__ == '__main__':
     
-    sucees_fix_folder = 'data/fsdata_2l'
-    fail_fix_folder = 'data/ffdata_2l'
+    sucees_fix_folder = 'data/fsdata_test'
+    fail_fix_folder = 'data/ffdata_test'
     
     create_folder(sucees_fix_folder)
     create_folder(fail_fix_folder)
