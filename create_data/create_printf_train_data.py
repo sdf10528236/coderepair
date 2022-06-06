8e35ccc19c963f500dc89bdaff9e705e4eb0ea2d
sdf10528236 <sdf10528236@gmail.com>
2022-06-06 23:14:36 +0800

    a

--- create_data/create_printf_train_data.py
+++ create_data/create_printf_train_data.py
@@ -14,11 +14,11 @@ def creat_printf(numbers, number_of_strings=20):  # 產生printf("字串")程式
 
     return cur_line_str
 
-def printf_parameter_string(cur_line_str, numbers):  # ex. printf("%d + asd %d ", a, b);
+def printf_parameter_string(cur_line_str, numbers):  # ex. printf("asdasd %d %d ", a, b);
     format_placeholder = ["d", "f","lf"]
-    
+    choice = ['0','1']
     format_placeholder = random.choice(format_placeholder)
-    op = ["+","-","*","="]
+   
     positions = [m.span()
                  for m in regex.finditer("\"", cur_line_str)]
     # print(cur_line_str, positions)
@@ -27,18 +27,12 @@ def printf_parameter_string(cur_line_str, numbers):  # ex. printf("%d + asd %d "
     cur_line_str = cur_line_str[:to_corrupt[0]]
 
     for i in range(numbers):
+        if int(random.choices(choice)[0]):
+            cur_line_str = cur_line_str+" "+"".join(random.choice(string.ascii_letters)
+                                                for _ in range(random.randint(0, 10)))
         cur_line_str = cur_line_str + \
             " %" + format_placeholder
-        if i < numbers-1: 
-            for i in range(random.randint(0,1)):
-                cur_line_str = cur_line_str+ " "+"".join(random.choice(
-            op))
-            for i in range(random.randint(0,1)):
-                if random.randint(0,1):
-                    cur_line_str = cur_line_str+" "+"".join(random.choice(
-                    string.ascii_letters)for _ in range(random.randint(1, 3)))
-                else:
-                    cur_line_str = cur_line_str+" "+"".join(str(random.randint(1, 20)))
+       
         
 
     cur_line_str = cur_line_str + "\""
@@ -136,42 +130,7 @@ def printf_add_parameter_op(cur_line_str, numbers):  # ex. #printf(" %d %d", p/x
 
 
 
-def printf_add_op(cur_line_str):  # ex.  printf("Pokah*123+WDM*f-Tt\n");
-    op = ["+","-","*","/","="]
-    cur_line_str = "printf(\"\");"
-    #------------------
-    positions = [m.span()
-                 for m in regex.finditer("\"", cur_line_str)]
-    to_corrupt = positions[1]  # 第2個"的地方
-    cur_line_str = cur_line_str[:to_corrupt[0]]+"".join(random.choice(string.ascii_letters)
-                                                for _ in range(random.randint(1, 10)))+cur_line_str[to_corrupt[0]:]
-    #------------------                                            
-    
-    positions = [m.span()
-                 for m in regex.finditer("\"", cur_line_str)]
-    to_corrupt = positions[1]  # 第2個"的地方
-    cur_line_str = cur_line_str[:to_corrupt[0]]
-    for i in range(random.randint(0,5)):
-        if random.randint(0,1):
-            cur_line_str = cur_line_str+" "+"".join(random.choice(
-                op))
-        if random.randint(0,1):
-            cur_line_str = cur_line_str+" "+"".join(random.choice(
-                string.ascii_letters)for _ in range(random.randint(1, 3)))
-        else:
-            cur_line_str = cur_line_str+" "+"".join(str(random.randint(1, 20)))
-            
-    cur_line_str = cur_line_str+"\");"
-    #------------------
 
-    positions = [m.span()
-                 for m in regex.finditer("\"", cur_line_str)]
-    # print(cur_line_str, positions)
-    to_corrupt = positions[1]  # 第二個"的地方
-    for i in range(random.randint(0,1)):
-        cur_line_str = cur_line_str[:to_corrupt[0]]+"\\n"+cur_line_str[to_corrupt[0]:]
-    print(cur_line_str)
-    return cur_line_str 
 
 def printf_string(cur_line_str, numbers):  # 產生printf("字串 字串 字串")程式碼    ex. printf("sdfsdgqw sdf sdfsdf");
     positions = [m.span()
@@ -205,13 +164,13 @@ if __name__ == '__main__':
 
     #--------------------------------------------------------------------------------------------
     cur_line_strs = []
-    cur_line_strs = creat_printf(5000, 0)
+    cur_line_strs = creat_printf(10000, 0)
     for i in range(len(cur_line_strs)):
         cur_line_strs[i] = printf_add_parameter(
-            cur_line_strs[i], random.randint(1, 3))    #printf(" %d %d %d", wPX, v, NIn);
+            cur_line_strs[i], random.randint(1, 5))    #printf(" %d %d %d", wPX, v, NIn);
     for cur_line_str in cur_line_strs:
         
-        quit()
+        
         cur_line_str_correct = cur_line_str
         for i in range(random.randint(5, 10)):
             cur_line_str_wrong = cur_line_str_correct
@@ -229,10 +188,10 @@ if __name__ == '__main__':
 
     #--------------------------------------------------------------------------------------------
     cur_line_strs = []
-    cur_line_strs = creat_printf(5000, 0)
+    cur_line_strs = creat_printf(10000, 0)
     for i in range(len(cur_line_strs)):
         cur_line_strs[i] = printf_parameter_string(
-            cur_line_strs[i], random.randint(1, 4))    #printf(" %d %d + %d", mof, U, vNv);
+            cur_line_strs[i], random.randint(1, 5))    #printf("asdasd %d %d ", a, b);
     for cur_line_str in cur_line_strs:
         
         
@@ -252,52 +211,11 @@ if __name__ == '__main__':
             cnt = cnt+1
     #--------------------------------------------------------------------------------------------
 
-    cur_line_strs = []
-    cur_line_strs = creat_printf(5000, 0)
-    for i in range(len(cur_line_strs)):
-        cur_line_strs[i] = printf_add_parameter_op(
-            cur_line_strs[i], random.randint(1, 3))    #printf(" %d %d", p/33, V+GMp-H/x);
-    for cur_line_str in cur_line_strs:
-        
-        
-        cur_line_str_correct = cur_line_str
-        for i in range(random.randint(5, 10)):
-            cur_line_str_wrong = cur_line_str_correct
-            for i in range(random.randint(1, 2)):
-                cur_line_str_wrong = auto_corrupt_syntax(
-                    cur_line_str_wrong)
-            
-
-            df = df.append({
-                "correct": cur_line_str_correct,
-                "wrong": cur_line_str_wrong,
-            }, ignore_index=True)
-            print(cnt)
-            cnt = cnt+1
-
 
     #--------------------------------------------------------------------------------------------
-    cur_line_strs = creat_printf(5000, 0)
-    for i in range(len(cur_line_strs)):
-        cur_line_strs[i] = printf_add_op(          #printf("Ugnj / 10 m = iDj * 4\n");
-            cur_line_strs[i])
-    for cur_line_str in cur_line_strs:
-        
-        cur_line_str_correct = cur_line_str
-        for i in range(random.randint(5, 10)):
-            cur_line_str_wrong = cur_line_str_correct
-            for i in range(random.randint(1, 2)):
-                cur_line_str_wrong = auto_corrupt_syntax(
-                    cur_line_str_wrong)
-            
-            df = df.append({
-                "correct": cur_line_str_correct,
-                "wrong": cur_line_str_wrong,
-            }, ignore_index=True)
-            print(cnt)
-            cnt = cnt+1
+    
     #--------------------------------------------------------------------------------------------------------------------
-    cur_line_strs = creat_printf(5000)               #printf("ZldxM YTQr Kdt JxoSjTm\n");
+    cur_line_strs = creat_printf(10000)               #printf("ZldxM YTQr Kdt JxoSjTm\n");
     for i in range(len(cur_line_strs)):
         cur_line_strs[i] = printf_string(
             cur_line_strs[i], random.randint(1, 5))
