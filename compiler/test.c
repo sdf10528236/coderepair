1ae44259470e9bfedce593d728af2c9c708379be
sdf10528236 <sdf10528236@gmail.com>
2022-06-15 12:44:16 +0800

    a

---
+++ compiler/test.c
@@ -0,0 +1,8 @@
+  #include <stdio.h>                                                                
+                                                                                                          
+  int main ( )                                                                             
+  {          
+      int smax,t;                                                                             
+   printf ( " %d %d, smax, t " ) ;                                                                    
+   return 0 ;                                                                     
+}   
